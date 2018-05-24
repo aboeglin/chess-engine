a73bfb4a42cbbd625d414dce8eb5eb28f945d9f1
Arnaud BOEGLIN <a.boeglin67@gmail.com>
2018-05-24 23:15:31 +0200

    Reformat canPawnMoveTo

--- src/rules.ts
+++ src/rules.ts
@@ -8,11 +8,11 @@ const canMoveTo = (x: string, y: string, piece: Piece, board: Board): boolean =>
 };
 
 const canPawnMoveTo = (x: string, y: string, piece: Piece, board: Board): boolean =>
-	parseInt(y) - parseInt(piece.y) === 1 && piece.color === Color.WHITE && !findPiece(x, y, board)
-	|| parseInt(y) - parseInt(piece.y) === -1 && piece.color === Color.BLACK && !findPiece(x, y, board)
-	|| (piece.y === '2' || piece.y === '7') && parseInt(y) - parseInt(piece.y) === 2 && piece.color === Color.WHITE
-		&& !findPiece(x, `${parseInt(y) - 1}`, board) && !findPiece(x, y, board)
-	|| (piece.y === '2' || piece.y === '7') && parseInt(y) - parseInt(piece.y) === -2 && piece.color === Color.BLACK
-		&& !findPiece(x, `${parseInt(y) + 1}`, board) && !findPiece(x, y, board);
+	parseInt(y) - parseInt(piece.y) === 1 && piece.color === Color.WHITE && !findPiece(x, y, board) ||
+	parseInt(y) - parseInt(piece.y) === -1 && piece.color === Color.BLACK && !findPiece(x, y, board) ||
+	parseInt(y) - parseInt(piece.y) === 2 && piece.color === Color.WHITE && (piece.y === '2' || piece.y === '7')
+	&& !findPiece(x, `${parseInt(y) - 1}`, board) && !findPiece(x, y, board) ||
+	parseInt(y) - parseInt(piece.y) === -2 && piece.color === Color.BLACK && (piece.y === '2' || piece.y === '7')
+	&& !findPiece(x, `${parseInt(y) + 1}`, board) && !findPiece(x, y, board);
 
 export {canMoveTo};
