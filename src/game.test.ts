06c92493b1bb6459993447a64f2e8c78d09f349d
Arnaud BOEGLIN <a.boeglin67@gmail.com>
2018-05-28 22:25:52 +0200

    Add first implementation of move
    TODO: more cases need to be handled.
    
    Should it take care of making sure the right player is playing ? Or should that be part of the consumer's code ?

--- src/game.test.ts
+++ src/game.test.ts
@@ -25,4 +25,32 @@ describe('game', () => {
 			expect(game.player2).toBe(p2);
 		});
 	});
+
+	describe('move', () => {
+		it('should export a function move', () => {
+			expect(typeof move).toBe('function');
+		});
+
+		it('should return a game object', () => {
+			const p1: Player = createPlayer('id-1', Color.WHITE);
+			const p2: Player = createPlayer('id-2', Color.BLACK);
+
+			const game: Game = createGame(p1, p2);
+			const afterMove: Game = move(createMove({x: 'd', y: '2'}, {x: 'd', y: '4'}), game);
+
+			expect(findPiece('d', '4', afterMove.board)).toEqual({type: PieceType.PAWN, color: Color.WHITE, x: 'd', y:'4'});
+		});
+
+		it('should remove a piece that has been killed', () => {
+			const p1: Player = createPlayer('id-1', Color.WHITE);
+			const p2: Player = createPlayer('id-2', Color.BLACK);
+
+			const game: Game = createGame(p1, p2);
+			const afterFirstMove: Game = move(createMove({x: 'd', y: '2'}, {x: 'd', y: '4'}), game);
+			const afterSecondMove: Game = move(createMove({x: 'e', y: '7'}, {x: 'e', y: '5'}), afterFirstMove);
+			const afterThirdMove: Game = move(createMove({x: 'd', y: '4'}, {x: 'e', y: '5'}), afterSecondMove);
+
+			expect(findPiece('e', '5', afterThirdMove.board)).toEqual({type: PieceType.PAWN, color: Color.WHITE, x: 'e', y:'5'});
+		});
+	});
 });
--- src/game.ts
+++ src/game.ts
@@ -1,5 +1,9 @@
-import {Board, createBoard} from './board';
-import {Player, makePlayer} from './player';
+import {Board, createBoard, findPiece, removePieceAt} from './board';
+import {Player} from './player';
+import {Piece} from './pieces';
+import {Move} from './move';
+import {Position} from './position';
+import {compose, concat} from './utils';
 
 interface Game {
 	board: Board;
@@ -17,4 +21,22 @@ const createGame = (player1: Player, player2: Player): Game => ({
 	redo: [],
 });
 
-export {createGame, Game};
+const move = (m: Move, game: Game): Game => {
+	const pieceAtDestination = findPiece(m.to.x, m.to.y, game.board);
+	const pieceToMove = findPiece(m.from.x, m.from.y, game.board);
+
+	return {
+		board: {
+			pieces: compose(cConcat({...pieceToMove, x: m.to.x, y: m.to.y}), cRemovePieceAt(m.from), cRemovePieceAt(m.to))(game.board.pieces),
+		},
+		undo: concat(game.board, game.undo),
+		redo: game.redo,
+		player1: game.player1,
+		player2: game.player2,
+	};
+};
+
+const cRemovePieceAt =  (position: Position) => (pieces: Piece[]) => removePieceAt(position, pieces);
+const cConcat = (value: any) => (arr: any[]) => concat(value, arr);
+
+export {createGame, Game, move};
